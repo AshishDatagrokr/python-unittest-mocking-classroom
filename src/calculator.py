f196cbae9d10f17675d3a0a8e6d28aa0dfe52192
Ashish <Ashish.srivastava@datagrokr.com>
2020-01-13 15:53:19 +0530

    Mocking Operation

--- src/calculator.py
+++ src/calculator.py
@@ -1,6 +1,29 @@
-class Calculator:
-    def sum(self, a, b):
-        return a + b
+from unittest import TestCase
+from unittest.mock import patch
+from src.calculator import Calculator
 
-    def difference(self, a, b):
-        return a - b
+class TestCalculator(TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_sum_without_mocking(self):
+        calculator = Calculator()
+        actual = calculator.sum(2, 4)
+        self.assertEqual(6, actual)
+
+    @patch('src.calculator.Calculator')
+    def test_sum_with_mocking(self, MockCalculator):
+        calculator = MockCalculator() 
+
+               calculator.sum.return_value = 6  
+
+        actual = calculator.sum(2, 4)  
+        expected = 6
+        
+        self.assertEqual(expected, actual)
+
+        
+        calculator.sum.return_value = 10
+        actual = calculator.sum(1, 1)
+        expected = 0
+        self.assertEqual(expected, actual)
--- src/db_helper.py
+++ src/db_helper.py
@@ -1,15 +1,54 @@
+import mysql.connector
+
 class DbHelper:
+
+    def get_connection():    
+    """ Connect to MySQL database """
+    conn = None
+    try:
+        conn = mysql.connector.connect(host=hostip,
+                                       database=database,
+                                       user=user,
+                                       password=pswd)
+        if conn.is_connected():
+            print('Connected to MySQL database')
+ 
+    except Error as e:
+        print(e)
+    return conn
+
+
+
+
+    def __init__(self):
+        self.conn = get_connection()
+        self.cursor = self.conn.cursor()
+
     def get_maximum_salary(self):
         '''
         Implement the logic to find and return maximum salary from employee table
         '''
-        return None
+        query = 'select max(salary) as max_salary from salaries'
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result[0]
 
     def get_minimum_salary(self):
         '''
         Implement the logic to find and return minimum salary from employee table
         '''
-        return None
+        query = 'select min(salary) as min_salary from salaries'
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        return result[0]
+    
+    def max_salary_is_greater_than_min(self):
+        max_salary = self.get_maximum_salary()
+        min_salary = self.get_minimum_salary()
+        if max_salary > min_salary:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
     db_helper = DbHelper()
